57989463db3d7a219198ce7bc5330d19de4014b0
Sarah Preston <seprest@gmail.com>
2022-08-14 14:41:23 -0500

    created music library function with JS to add and search for records

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,21 +1,27 @@
-// console.log('***** Music Collection *****')
+//---------------------------------------------------------------------------------------------------------//
+// YAY PSEUDOCODE !                                                                                        //
+//---------------------------------------------------------------------------------------------------------//
+
+// Itialize collection array
+//
+// Create function to add albums to the collection array (using paramters about title, artist and year published{
+//      initialize variable record = {
+//          set,
+//          properties,
+//          of the variable
+//      }
+//      add the new record to the collection;
+//      return the record;
+//}
 
 
-// - Create a variable `collection` that starts as an empty array.
-
-// - Add a function named `addToCollection`. This function should:
-//   - Take in the album's `title`, `artist`, `yearPublished` as input parameters
-//   - Create a new object having the above properties
-//   - Add the new object to the end of the `collection` array
-//   - Return the newly created object
 
 
 
 let collection = [];
-let record;
 
 function addToCollection(title, artist, yearPublished){
-    record = {
+    let record = {
         title,
         artist,
         yearPublished
@@ -24,35 +30,18 @@ function addToCollection(title, artist, yearPublished){
     return record;
 }
 
-
-// - Test the `addToCollection` function:
-//   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
-//   - Console.log each album as added using the returned value.
-//   - After all are added, console.log the `collection` array.
-
-
-addToCollection ("Pet Sounds", "The Beach Boys", 1966);
-console.log(record);
-addToCollection ("What's Going On", "Marvin Gaye", 1971);
-console.log(record);
-addToCollection ("Tapestry", "Carole King", 1972);
-console.log(record);
-addToCollection ("Rise and Fall of Ziggy Stardust", "David Bowie", 1972);
-console.log(record);
-addToCollection ("Out of the Blue", "Electric Light Orchestra", 1977);
-console.log(record);
-addToCollection ("Graceland", "Paul Simon", 1986);
-console.log(record);
-
+console.log(addToCollection("Pet Sounds", "The Beach Boys", 1966));
+console.log(addToCollection("What's Going On", "Marvin Gaye", 1971));
+console.log(addToCollection("Tapestry", "Carole King", 1972));
+console.log(collection);
+    //When I do this, during this adding process, it will say ---> (3) [{…}, {…}, {…}],
+    //which shows an array of three objects, but when I open it up, I see the full collection,
+    //with all six albums. I'm not sure why it does this.
+console.log(addToCollection("Rise and Fall of Ziggy Stardust", "David Bowie", 1972));
+console.log(addToCollection("Out of the Blue", "Electric Light Orchestra", 1977));
+console.log(addToCollection("Graceland", "Paul Simon", 1986));
 console.log(collection);
 
-
-
-// - Add a function named `showCollection`. This function should:
-//   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
-//   - Console.log the number of items in the array.
-//   - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
-// - Test the `showCollection` function.
 
 function showCollection(array){
     console.log(array.length);
@@ -64,55 +53,37 @@ function showCollection(array){
 showCollection(collection);
 
 
-
-
-
-// - Add a function named `findByArtist`. This function should:
-//   - Take in `artist` (a string) parameter
-//   - Create an array to hold any results, empty to start
-//   - Loop through the `collection` and add any objects with a matching artist to the array.
-//   - Return the array with the matching results. If no results are found, return an empty array.
-
-// - Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are found.
-
-// > When testing your functions, write all tests in the JavaScript file!
-
 function findByArtist(artist){
-    let albums = [];
+    let results = [];
     for (record of collection){
         if (artist === record.artist){
-            albums.push(record);
+            results.push(record);
         }
     }
-    return albums;
+    return results;
 }
 
-console.log('The Beach Boys' - findByArtist('The Beach Boys'));
-console.log('The Beatles' - findByArtist('The Beatles'));
+console.log('The Beach Boys -', findByArtist('The Beach Boys'));
+console.log('Paul Simon  -', findByArtist('Paul Simon'));
 addToCollection ("Surfer Girl", "The Beach Boys", 1963);
 console.log(collection);
-console.log('The Beach Boys' - findByArtist('The Beach Boys'));
+console.log('The Beach Boys -', findByArtist('The Beach Boys'));
 
+
+//---------------------------------------------------------------------------------------------------------//
 // ### Stretch goals
-
-// - Create a function called `search`. This function should:
-//   - Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
-//   ```
-//   { artist: 'Ray Charles', year: 1957 }
-//   ```
-//   - The returned output from `search` should meet these requirements:
-//     - Return a new array of all items in the `collection` matching *all* of the search criteria.
-//     - If no results are found, return an empty array.
-//     - If there is no search object or an empty search object provided as input, then return all albums in the `collection`.
+//---------------------------------------------------------------------------------------------------------//
 
 function search(item){
     let results = [];
-    for (record of collection){
-        if (item === record.title || item === record.artist || item === record.yearPublished){
+    if (item){ //if there's a search parameter
+        for (record of collection){
+            if (item === record.title || item === record.artist || item === record.yearPublished){
             results.push(record);
+                }
             }
         }
-    if (item === []){
+    else { //if there isn't a search parameter
         return collection;
     }
     return results;
@@ -123,10 +94,8 @@ console.log('Ray Charles -', search('Ray Charles'));
 console.log('Marvin Gaye -', search('Marvin Gaye'));
 console.log('1972 -', search(1972));
 console.log('Graceland -', search('Graceland'));
+console.log('Wrong Item -', search('Wrong'));
 console.log('Empty -', search());
-///Not really sure how to fix this last bit.
-
-
 
 
 // - Add an array of `tracks` to your album objects. Each track should have a `name` and `duration`. You will need to update the functions to support this new property:
@@ -144,52 +113,43 @@ console.log('Empty -', search());
 // ```
 
 
-//These updates seems to be messing up my answers from before, so I'm just going to comment it out, 
-//after testing them out to make sure they work.
+//---------------------------------------------------------------------------------------------------------//
 
 
+let musicLibrary = [{title: 'Pet Sounds', artist: 'The Beach Boys', yearPublished: 1966}]; 
+//Created, in order to preserve my results for the original functions
+
+console.log(musicLibrary);
 let tracks = [];
-song = {trackName, duration};
-
-function addToCollection(title, artist, yearPublished, trackName, duration){
-    let tracks = [];
-    record = {
-        title,
-        artist,
+function addToLibrary(title, artist, yearPublished, trackName, duration){
+    let album = {
+        title, 
+        artist, 
         yearPublished,
-        tracks 
-    }
+        tracks
+    };
 
-    tracks.push(song={trackName, duration});
-    collection.push(record);
-    return console.log(record);
-    
-}
+tracks.push({trackName,duration});
 
-
-
-addToCollection ("Pet Sounds", "The Beach Boys", 1966, "Wouldn't It Be Nice", 6.66);
-addToCollection ("Pet Sounds", "The Beach Boys", 1966, 'God Only Knows', 6.66);
-addToCollection ("Pet Sounds", "The Beach Boys", 1966, 'Good Vibrations', 6.66);
-addToCollection ("Graceland", "Paul Simon", 1986, 'Diamonds on the Soles of Her Shoes', 4.44);
-addToCollection ("Graceland", "Paul Simon", 1986, 'You Can Call Me Al', 4.44);
-
-
-function search(item){
-    let results = [];
-    for (record of collection){
-        if (item === record.title || item === record.artist || item === record.yearPublished || item === song.trackName){
-            results.push(record);
-            }
+    for (i=0; i<musicLibrary.length; i++){ //loops through albums 
+        if (musicLibrary.indexOf(album.title !== 0 )){ //sees if there's an album match
+            console.log('help');
         }
-    if (item === []){
-        return collection;
-    }
-    return results;
- }
+    }//end of loop
+
+return console.log(musicLibrary); 
+}
+addToLibrary("Pet Sounds", "The Beach Boys", 1966, "Wouldn't It Be Nice", 6.06);
+addToLibrary("Pet Sounds", "The Beach Boys", 1966, 'God Only Knows', 2.26);
+addToLibrary("Graceland", "Paul Simon", 1986, 'Diamonds on the Soles of Her Shoes', 4.41);
+addToLibrary("Graceland", "Paul Simon", 1986, 'You Can Call Me Al', 2.34);
 
 
-console.log('Diamonds' - search('Diamonds on the Soles of Her Shoes'));
-console.log('Good Vibrations' - search('Good Vibrations'));
-console.log('The Beach Boys' - search('The Beach Boys'));
-console.log('Graceland' - search('Graceland'));
+
+
+// I understand that the question was asking met to add an array of tracks to a particular album, 
+// but I got stuck on wanting to add a single track, to an array of tracks, as a property of the album.
+// It sometimes would work, but all of the tracks would end up grouped together, which lead me down a 
+// rabbit-hole of researching groupBy/reduce and various other methods, which I just don't feel like I 
+// have the foundational understanding to use. So I'm going to stop here! This is the remnants of my 
+// attempt.
